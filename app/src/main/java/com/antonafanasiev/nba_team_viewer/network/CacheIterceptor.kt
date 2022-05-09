831300cfeaaf8a4b737b5e0a0cd017dcc5f04115
aafanasi <Anton.Afanasiev@genesys.com>
2022-05-09 16:29:32 -0300

    Hi

--- app/src/main/java/com/antonafanasiev/nba_team_viewer/network/CacheIterceptor.kt
+++ app/src/main/java/com/antonafanasiev/nba_team_viewer/network/CacheIterceptor.kt
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit
 
 class CacheInterceptor : Interceptor {
 
+    // Hi
     override fun intercept(chain: Interceptor.Chain): Response {
         val response = chain.proceed(chain.request())
 
